69beb81b2c4bcd159be8b475f0bf93deec4eb4e5
sikard <59831297+sikard@users.noreply.github.com>
2020-06-09 16:04:11 +0300

    Initial Commit

---
+++ save/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  save
+//
+//  Created by Arda Şıklaroğlu on 9.06.2020.
+//  Copyright © 2020 Arda Şıklaroğlu. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
